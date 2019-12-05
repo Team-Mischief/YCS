1c5c5f770cb92e36cbee507169b64005dcb00064
jurczakn <nickolas.jurczak@gmail.com>
2019-12-05 10:11:43 -0500

    added react-bootstrap

--- ui/ycs-ui/src/App.js
+++ ui/ycs-ui/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, ButtonToolbar } from 'react-bootstrap';
 
 function App() {
   return (
@@ -19,6 +21,17 @@ function App() {
           Learn React
         </a>
       </header>
+      <ButtonToolbar>
+        <Button variant="primary">Primary</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="success">Success</Button>
+        <Button variant="warning">Warning</Button>
+        <Button variant="danger">Danger</Button>
+        <Button variant="info">Info</Button>
+        <Button variant="light">Light</Button>
+        <Button variant="dark">Dark</Button>
+        <Button variant="link">Link</Button>
+      </ButtonToolbar>
     </div>
   );
 }
