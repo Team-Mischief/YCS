afe13eff8b8236dc27bf1eed6de97d5306478fd8
jurczakn <nickolas.jurczak@gmail.com>
2019-12-05 11:29:15 -0500

    did the routing thing

--- ui/ycs-ui/src/index.js
+++ ui/ycs-ui/src/index.js
@@ -4,24 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<><link
-    rel="stylesheet"
-    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-    crossorigin="anonymous"
-  /><App /><script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin />
-
-  <script
-    src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-    crossorigin
-  />
-  
-  <script
-    src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-    crossorigin
-  />
-  
-  <script>var Alert = ReactBootstrap.Alert;</script></>, document.getElementById('root'));
+ReactDOM.render(<><App /><script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin />
+</>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
