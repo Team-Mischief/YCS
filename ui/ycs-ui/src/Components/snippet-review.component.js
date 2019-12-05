bc328b15f83aa3649f6297b9785e6e3cceede1a7
Ricky Orr <rlayneorr@gmail.com>
2019-12-05 15:05:50 -0500

    Doing some stuff!

--- ui/ycs-ui/src/Components/snippet-review.component.js
+++ ui/ycs-ui/src/Components/snippet-review.component.js
@@ -1,10 +1,30 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
+import SnippetService from '../Services/snippet.service';
 
 class SnippetReview extends Component {
+    snippetService = new SnippetService();
 
+    constructor(props) {
+        super(props);
+        this.state = { codes: []};
+        this.snippetService.getSnippets().then(res => {
+            console.log(res);
+            this.setState({codes: res.data});
+        });
+    }
     render() {
-
-        return <h2>This is a Snippet Review page, I swear</h2>
+        return (
+            <>
+                {this.state.codes.map(code => (
+                    <div key={code.Id}>
+                        <h3>{code.dev.handle}</h3>
+                        <img src={code.dev.picture}></img>
+                        <div>{code.avgRating}</div>
+                        <p>{code.snippet}</p>
+                    </div>
+                ))}
+            </>
+        )
 
     }
 
